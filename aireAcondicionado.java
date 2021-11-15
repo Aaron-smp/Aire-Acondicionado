735af85b7e6d13baf6c9202ee6c4870788b4490c
Aarón Pérez Carreño <aapecargame@gmail.com>
2021-11-15 13:52:26 +0100

    0497.02 subir bajar temperatura

--- aireAcondicionado.java
+++ aireAcondicionado.java
@@ -1,4 +1,17 @@
 public class aireAcondicionado{
-
-
+    // Temperatura del agua
+    private float temperatura;
+    
+    public aireAcondicionado(){
+        temperatura = 15.0f;
+    }
+    public void subirTemperatura(){
+        temperatura += 5.0f;
+    }
+    public void bajarTemperatura(){
+        temperatura -= 5.0f;
+    }
+    public float getTemperatura(){
+        return temperatura;       
+    }
 }
