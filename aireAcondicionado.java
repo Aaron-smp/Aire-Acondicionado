1181edcbb569417f637ca9efec812134e48cd5e4
Aarón Pérez Carreño <aapecargame@gmail.com>
2021-11-16 08:52:34 +0100

    0497.03 subir y bajar la temperatura

--- aireAcondicionado.java
+++ aireAcondicionado.java
@@ -1,15 +1,38 @@
 public class aireAcondicionado{
     // Temperatura del agua
     private double temperatura;
-    
-    public aireAcondicionado(){
+    // Maximo de temperatura permitida
+    private double max;
+    // Minimo de temperatura permitida
+    private double min;
+    // Incremento sera la cantida de temperatura que aumente
+    private double incremento;
+    public aireAcondicionado(double temperaturaMax, double temperaturaMin){
+        max = temperaturaMax;
+        min = temperaturaMin;
+        incremento = 5.0;
         temperatura = 15.0;
     }
     public void subirTemperatura(){
-        temperatura += 5.0;
+        if(temperatura < max){
+            temperatura += incremento;
+        }
+        if(temperatura > max){
+            temperatura = max;
+        }
     }
     public void bajarTemperatura(){
-        temperatura -= 5.0;
+        if(temperatura > min){
+            temperatura -= incremento;
+        }
+        if(temperatura < min){
+            temperatura = min;
+        }
+    }
+    public void setIncremento(double newIncremento){
+        if(newIncremento > 0){
+            incremento = newIncremento;
+        }
     }
     public double getTemperatura(){
         return temperatura;       
